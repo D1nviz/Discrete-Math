a0c30838b3130b2b5e9ee9c4ba7d35665b9ae649
Dinviz <eugene.hetsyanyn@gmail.com>
2023-12-27 19:59:50 +0200

    update

--- session-results.cpp
+++ session-results.cpp
@@ -1,28 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    // Total students who got at least one of each grade
-    int satisfactory = 13;
-    int good = 13;
-    int excellent = 12;
+int32_t main() {
+  int a, b, c, good_and_excellent, satisfactory_and_excellent, satisfactory_and_good, all_grades;
+  cout << "enter 1 mark ."; cin >> a;
+  cout << "enter 2 mark ."; cin >> b;
+  cout << "enter 3 mark ."; cin >> c;
+  cout << "enter 4 value ."; cin >> good_and_excellent;
+  cout << "enter 5 value ."; cin >> satisfactory_and_excellent;
+  cout << "enter 6 value ."; cin >> satisfactory_and_good;
+  cout << "enter 7 all ."; cin >> all_grades;
 
-    // Students who got both grades
-    int good_and_excellent = 4;
-    int satisfactory_and_excellent = 5;
-    int satisfactory_and_good = 4;
+  int only_satisfactory = a - satisfactory_and_good - satisfactory_and_excellent + all_grades;
+  int only_good = b - satisfactory_and_good - good_and_excellent + all_grades;
+  int only_excellent = c - satisfactory_and_excellent - good_and_excellent + all_grades;
 
-    // Students who got all three grades
-    int all_grades = 3;
+  int result = 100 * only_excellent + 10 * only_good + only_satisfactory;
 
-    // Calculate students who got only one grade
-    int only_satisfactory = satisfactory - satisfactory_and_good - satisfactory_and_excellent + all_grades;
-    int only_good = good - satisfactory_and_good - good_and_excellent + all_grades;
-    int only_excellent = excellent - satisfactory_and_excellent - good_and_excellent + all_grades;
-
-    // Calculate the expression
-    int result = 100 * only_excellent + 10 * only_good + only_satisfactory;
-
-    std::cout << "The result is: " << result << std::endl;
-
-    return 0;
+  cout << endl << "The result is: " << result << endl;
 }
